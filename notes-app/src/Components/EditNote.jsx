a4750bd65976daad105f3bb0ed13154d9589bc66
Manas Singh <manasinghofficial@gmail.com>
2025-08-01 11:31:15 +0530

    commit

--- notes-app/src/Components/EditNote.jsx
+++ notes-app/src/Components/EditNote.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditNote = (notes,setnotes) => {
+    const navigate = useNavigate()
     const params = useParams()
   return (
     <>
-        <button className='edit-btn'>Edit</button>
+       
     </>
   )
 }
--- notes-app/src/Components/NoteList.jsx
+++ notes-app/src/Components/NoteList.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import EditNote from "./EditNote";
+import { Link, useNavigate } from "react-router-dom";
 
 const NoteList = ({ notes, setnotes }) => {
+
+  const navigate = useNavigate()
   const deleteHandler = (id) => {
     const updatednotes = notes.filter((note) => note.id !== id);
     setnotes(updatednotes);
   };
 
+  const editHandler=()=>{
+    navigate(`/editnote/${note.id}`)
+  }
+
   const rendernotes = notes.map((note) => {
     return (
       <div key={note.id} className="note">
         <div className="btns">
-          <EditNote notes={notes} setnotes={setnotes} />
+          <Link className="edit-btn" to={`/editnote/${note.id}`}>Edit</Link>
           <button className="delete-btn" onClick={() => deleteHandler(note.id)}>
             delete
           </button>
@@ -25,6 +32,7 @@ const NoteList = ({ notes, setnotes }) => {
     <>
       <h1>All Notes</h1>
       <div className="notes-cont">{rendernotes}</div>
+      {/* <EditNote notes={notes} setnotes={setnotes} id={note.id} /> */}
     </>
   );
 };
